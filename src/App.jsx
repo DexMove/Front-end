84e4aa97abe52e4f78a3da17e131ae8bab55c70e
melissafiuza <melissafiuza153@gmail.com>
2026-05-17 13:21:46 -0300

    area bloqueada

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./componentes/Header";
 import Planos from "./componentes/pages/planos/Planos";
 import Produtos from "./componentes/pages/produtos/Produtos";
+import Bloqueada from "./componentes/pages/bloqueada/Bloqueada";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         
         <Route path="/planos" element={<Planos />} />
         <Route path="/produtos" element={<Produtos />} />
+        <Route path="/area-bloqueada" element={<Bloqueada />} />
       </Routes>
     </BrowserRouter>
   );
