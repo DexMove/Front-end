c1399c88d60035d819d218bf817512ad825adaf0
melissafiuza <melfiuza153@gmail.com>
2026-06-15 00:59:43 -0300

    ajuses

--- src/peges1/Home/Home.jsx
+++ src/peges1/Home/Home.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import Footer from "../../componentes/Footer";
-import Section_1 from "./Componentes/section_1";
-import Section_2 from "./Componentes/section_2";
-import Section_3 from "./Componentes/section_3";
-import Section_4 from "./Componentes/section_4";
-import Section_5 from "./Componentes/section_5";
-import Section_6 from "./Componentes/section_6";
-import Section_7 from "./Componentes/section_7";
+import Section_1 from "./Componentes/Section_1";
+import Section_2 from "./Componentes/Section_2";
+import Section_3 from "./Componentes/Section_3";
+import Section_4 from "./Componentes/Section_4";
+import Section_5 from "./Componentes/Section_5";
+import Section_6 from "./Componentes/Section_6";
+import Section_7 from "./Componentes/Section_7";
 import SectionFisio from "./Componentes/SectionFisio";
 import styles from './Home.module.css'
 
